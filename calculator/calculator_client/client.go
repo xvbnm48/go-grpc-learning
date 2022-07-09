0e9f326cea47480a5cc80a3f483c40e0ea4e9a7e
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-07-09 23:30:18 +0700

    server streaming calculator

--- calculator/calculator_client/client.go
+++ calculator/calculator_client/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 
 	"github.com/xvbnm48/go-grpc-learning/calculator/calculatorpb"
@@ -20,7 +21,8 @@ func main() {
 
 	c := calculatorpb.NewCalculatorServiceClient(cc)
 	// fmt.Printf("Created client: %v\n", c)
-	doUnary(c)
+	// doUnary(c)
+	doServerStreaming(c)
 }
 
 func doUnary(c calculatorpb.CalculatorServiceClient) {
@@ -35,4 +37,27 @@ func doUnary(c calculatorpb.CalculatorServiceClient) {
 	}
 
 	log.Printf("Response from sum: %v", res.SumResult)
+}
+
+func doServerStreaming(c calculatorpb.CalculatorServiceClient) {
+	fmt.Println("Starting to do a prime server streaming RPC...")
+	req := &calculatorpb.PrimeNumberDecompositionRequest{
+		Number: 129292828282,
+	}
+	stream, err := c.PrimeNumberDecomposition(context.Background(), req)
+	if err != nil {
+		log.Fatalf("error while calling prime decomposition RPC: %v", err)
+	}
+
+	for {
+		res, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("error while reading stream: %v", err)
+		}
+		fmt.Println(res.GetPrimeFactor())
+	}
+
 }
